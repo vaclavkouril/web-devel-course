59bf8e2840811d796d1fe51a43c747fb1b5c6d80
Vaclav Kouril <vaclav.kouril02@gmail.com>
2023-11-13 16:59:14 +0100

    Imported HW files

---
+++ homework/php-templator/templator.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Represents the template compiler.
+ */
+class Templator
+{
+    /**
+     * Load a template file into memory.
+     * @param string $fileName Path to the template file to be loaded.
+     */
+    public function loadTemplate(string $fileName)
+    {
+
+        // You need to implement this!
+
+    }
+
+    /**
+     * Compile the loaded template (transpill it into interleaved-PHP) and save the result in a file.
+     * @param string $fileName Path where the result should be saved.
+     */
+    public function compileAndSave(string $fileName)
+    {
+
+        // You need to implement this!
+
+    }
+}
