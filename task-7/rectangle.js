ccf95607990729bbfdbaedafad95845e3d030d05
Vaclav Kouril <vaclav.kouril02@gmail.com>
2023-11-28 18:43:41 +0100

    task-7 reworked rectangel writing

--- task-7/rectangle.js
+++ task-7/rectangle.js
@@ -1,10 +1,29 @@
+function Perimeter(a,b) {return 2*a+2*b;}
+function Area(a,b) {return a*b;}
+
+function SpecialLine(numofcolumns) {
+    let line = "";
+    for (let i = 0; i < numofcolumns; i++) {
+        if(i === 0 || i === numofcolumns-1){ line += "*";}
+        else {line += "-";}
+    }
+   return line
+}
+
+function NormalLine(numofcolumns) {
+    let line = "";
+    for (let i = 0; i < numofcolumns; i++) {
+        if(i === 0 || i === numofcolumns-1){ line += "|";}
+        else {line += "-";}
+    }
+   return line
+}
+
 function Sq(a,b) {
-    const perimeter = 2*a+2*b;
-    const area = a*b;
     
-    console.log("Perimeter: " + perimeter);
+    console.log("Perimeter: " + Perimeter(a,b));
     
-    console.log("Area: " + area);
+    console.log("Area: " + Area(a,b));
     
 
     for (let i = 0; i < b; i++) {
@@ -27,4 +46,20 @@ function Sq(a,b) {
     }
 }
 
+function SquareWriteReworked(sideA, sideB) {
+    console.log("Perimeter: " + Perimeter(sideA,sideB));
+    
+    console.log("Area: " + Area(sideA,sideB));
+    
+
+    for (let i = 0; i < sideB; i++) {
+        let line = "";
+        if (i===0 || i === sideB-1) {line = SpecialLine(sideA);}
+        else {line = NormalLine(sideA);}
+        console.log(line);
+    }
+
+}
+
 Sq(5,3);
+SquareWriteReworked(5,3);
